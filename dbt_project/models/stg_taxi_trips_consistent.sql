394971b5ca307248d4e82e52055ef6c1c05bd98b
Monica Wu <mango5sim@gmail.com>
2025-07-15 04:48:30 +0200

    Fix snowflake columns case-sensitive issue 5

--- dbt_project/models/stg_taxi_trips_consistent.sql
+++ dbt_project/models/stg_taxi_trips_consistent.sql
@@ -3,14 +3,14 @@
 ) }}
 
 SELECT
-    CAST("VendorID" AS INTEGER) AS vendor_id,
+    CAST("vendorid" AS INTEGER) AS vendor_id,
     CAST("tpep_pickup_datetime" AS TIMESTAMP) AS tpep_pickup_datetime,
     CAST("tpep_dropoff_datetime" AS TIMESTAMP) AS tpep_dropoff_datetime,
     CAST("passenger_count" AS INTEGER) AS passenger_count,
     CAST("trip_distance" AS NUMBER(10, 2)) AS trip_distance,
     CAST("pickup_longitude" AS NUMBER(18, 15)) AS pickup_longitude,
     CAST("pickup_latitude" AS NUMBER(18, 15)) AS pickup_latitude,
-    CAST("RatecodeID" AS INTEGER) AS ratecode_id,
+    CAST("ratecodeid" AS INTEGER) AS ratecode_id,
     CAST("store_and_fwd_flag" AS VARCHAR) AS store_and_fwd_flag,
     CAST("dropoff_longitude" AS NUMBER(18, 15)) AS dropoff_longitude,
     CAST("dropoff_latitude" AS NUMBER(18, 15)) AS dropoff_latitude,
